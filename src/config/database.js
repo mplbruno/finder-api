2aa6819b212cddcc7d646cc05dde990b8cbd8e38
mplbruno <mplbruno@gmail.com>
2022-07-14 11:17:05 -0300

    update pipeline

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+require('dotenv/config');
+
+module.exports = {
+  dialect: 'postgres',
+  port: 5432,
+  host: 'bd-prod.postgres.database.azure.com',
+  username: 'dbprod@bd-prod',
+  password: 'P88U6B%#!y1!',
+  database: 'postgres',
+  define: {
+    timeStamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  ssl: true,
+  sslmode: "required",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+}
